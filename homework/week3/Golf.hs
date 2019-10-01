b07d751b9bc604ac9018e719c7748de14e472614
Chris Speck <cgspeck@gmail.com>
2019-10-01 23:36:24 +1000

    Some boiler plate for a nieve implementation

--- homework/week3/Golf.hs
+++ homework/week3/Golf.hs
@@ -13,3 +13,18 @@ module Golf where
     | m > h && m > n = [m] ++ localMaxima xs
     | otherwise = localMaxima ms
   localMaxima _ = []
+
+  histogram :: [Integer] -> String
+  histogram = undefined
+
+  -- stuff that goes at the bottom, i.e. --- and column labels
+  _boilerplate :: [Char]
+  _boilerplate = undefined
+
+  -- look through [x] and count digits equal to num_to_tally
+  _findCount :: [Integer] -> Int -> Int
+  _findCount [x:xs] num_to_tally = _
+
+    -- look through [x], if x > threshold return a '*' otherwise a ' '
+  _countToLine :: [Integer] -> Int -> [Char]
+  _countToLine [x:xs] threshold = _
