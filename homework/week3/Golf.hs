d37a4593ab01370f13bcac51ed166ccd704b25d8
Chris Speck <cgspeck@gmail.com>
2019-10-01 18:03:13 +1000

    WIP

--- homework/week3/Golf.hs
+++ homework/week3/Golf.hs
@@ -1,9 +1,10 @@
 module Golf where
   skips :: [a] -> [[a]]
-  skips = undefined
+  skips x = map takeEvery x 1 [1..length x]
+  skips _ = []
   
   takeEvery :: [a] -> Integer -> Integer -> [a]
-  takeEvery (x:xs) takePos curPos
+  takeEvery (x:xs) curPos takePos 
     | (mod curPos takePos) == 0 = x : takeEvery xs takePos (curPos + 1)
     | otherwise = takeEvery xs takePos (curPos + 1)
   takeEvery [] _ _ = []
