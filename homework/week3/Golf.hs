bb8eba3a4621cf3ea37db91efcbe7ac90a915364
Chris Speck <cgspeck@gmail.com>
2019-10-01 22:12:07 +1000

    LocalMaxima function

--- homework/week3/Golf.hs
+++ homework/week3/Golf.hs
@@ -7,3 +7,9 @@ module Golf where
     | (mod curPos takePos) == 0 = x : takeEvery xs (curPos + 1) takePos
     | otherwise = takeEvery xs (curPos + 1) takePos
   takeEvery [] _ _ = []
+
+  localMaxima :: [Integer] -> [Integer]
+  localMaxima (h:ms@(m:xs@(n:t)))
+    | m > h && m > n = [m] ++ localMaxima xs
+    | otherwise = localMaxima ms
+  localMaxima _ = []
