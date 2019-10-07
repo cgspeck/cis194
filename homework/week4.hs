0bbf061ab26898fa6c99eca96bac7c8c3c21db28
Chris Speck <cgspeck@gmail.com>
2019-10-08 08:19:13 +1100

    Basic refactor of fun1

--- homework/week4.hs
+++ homework/week4.hs
@@ -1,13 +1,11 @@
+import Data.List
 
-{--
-fun1 :: [Integer] -> Integer
-fun1 [] = 1
-fun1 (x:xs)
+fun_1 :: [Integer] -> Integer
+fun_1 [] = 1
+fun_1 (x:xs)
   | even x = (x - 2) * fun1 xs
   | otherwise = fun1 xs
---}
 
--- don't forget to do the multiplication once this is working
 fun1 :: [Integer] -> Integer
 -- fun1 xs = foldl (subtract 2) 1 (filter(even xs))
-fun1 xs = foldl (subtract) 1 (filter even xs)
+fun1 xs = foldl'  ((*) . (\x -> x - 2)) 1 (filter even xs)
